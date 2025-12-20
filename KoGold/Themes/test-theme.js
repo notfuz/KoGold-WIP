6c182cb94f0145a8f46c0c789375165f500929c3
Fuz :3 <sharkinc0806@gmail.com>
2025-12-21 01:32:26 +0200

    Add files via upload

---
+++ KoGold/Themes/test-theme.js
@@ -0,0 +1,68 @@
+// Define container edits here
+
+window.CONTAINER_EDITS = [
+  {
+    selector: "body", // CSS selector
+    styles: {
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      background: "url(https://imgur.com/9ncCjcg.png)"
+    }
+  },
+
+  {
+    selector: ".row.pb-2.gamesContainer-0-2-33",
+    styles: {
+      backgroundColor: "#ffffff01"
+    }
+  },
+
+  {
+    selector: ".col-12.col-lg-3.advertisementContainer-0-2-76",
+    styles: {
+      display: "none"
+    }
+  },
+
+  {
+    selector: ".skyScraperLeft-0-2-33",
+    styles: {
+      display: "none"
+    }
+  },
+
+  {
+    selector: ".skyScraperRight-0-2-34",
+    styles: {
+      display: "none"
+    }
+  },
+
+  {
+    selector: ".col-12.feedNews-0-2-44",
+    styles: {
+      display: "none"
+    }
+  },
+
+  {
+    selector: ".col-12.col-lg-9.uselessFuckingClass-0-2-75",
+    styles: {
+      width: "100%"
+    }
+  },
+
+  {
+    selector: ".section-content",
+    styles: {
+        backgroundColor: "#ffffff01",
+        boxShadow: "none"
+    }
+  },
+
+  {
+    selector: ".containerHeader-0-2-73",
+    styles: {
+        width: "85.75%"
+    }
+  }
+];
